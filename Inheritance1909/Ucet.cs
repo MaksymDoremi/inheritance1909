b951c5fd8430e3ae849199944baad4f85291180e
Maksym Kintor <kintor@spsejecna.cz>
2022-09-19 08:15:18 +0200

    ucet commit bash

---
+++ Inheritance1909/Ucet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance1909
+{
+    internal class Ucet
+    {
+
+        public virtual void vklad()
+        {
+
+        }
+
+        public virtual void vyber()
+        {
+
+        }
+
+        public virtual void status()
+        {
+
+        }
+    }
+}
